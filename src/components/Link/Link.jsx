fd0bbc6adf377ae07749662a90400dd701ad95d1
Dan Moffat <danmoffat@gmail.com>
2024-03-19 02:16:54 +0000

    Finished logic for updating app location state

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,8 +1,13 @@
+import { useContext } from 'react';
+import { NavigationContext } from '../../context/NavigationContext';
 import PropTypes from 'prop-types';
 
 const Link = ({ children, to, ...rest }) => {
+  const { setLocation } = useContext(NavigationContext);
+
   const handleClick = e => {
     e.preventDefault();
+    setLocation(to);
     window.history.pushState({}, '', to);
   };
 
--- src/context/NavigationContext.jsx
+++ src/context/NavigationContext.jsx
@@ -1,10 +1,19 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 export const NavigationContext = createContext();
 
 const NavigationContextProvider = ({ children }) => {
-  const [location, setLocation] = useState('hello there');
+  const [location, setLocation] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => setLocation(window.location.pathname);
+
+    window.addEventListener('popstate', handler);
+
+    // Return cleanup as arrow function to avoid it running on initial load
+    return () => window.removeEventListener('popstate', handler);
+  }, []);
 
   return <NavigationContext.Provider value={{ location, setLocation }}>{children}</NavigationContext.Provider>;
 };
