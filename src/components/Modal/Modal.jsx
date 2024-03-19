1cb7670e70d2e09eb94d1f49da5f3e4c64b45918
Dan Moffat <danmoffat@gmail.com>
2024-03-19 16:01:00 +0000

    Added e.stopPropagation() to complete the modal window

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -20,7 +20,9 @@ const Modal = ({ message }) => {
           <div
             onClick={handleClick}
             className='fixed bg-black/50 top-0 left-0 w-full h-full flex items-center justify-center'>
-            <div className='bg-white p-8'>{message}</div>
+            <div className='bg-white p-8' onClick={e => e.stopPropagation()}>
+              {message}
+            </div>
           </div>,
           document.body
         )}
