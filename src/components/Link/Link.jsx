3844acdda2fadff914ab42626890e6afc8547e7c
Dan Moffat <danmoffat@gmail.com>
2024-03-19 03:43:10 +0000

    Refectored sidebar to map over an array instead of hardcoding links

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -2,7 +2,7 @@ import useNavigation from '../../hooks/useNavigation';
 import PropTypes from 'prop-types';
 
 const Link = ({ children, to, ...rest }) => {
-  const { navigate } = useNavigation();
+  const { location, navigate } = useNavigation();
 
   const handleClick = e => {
     e.preventDefault();
@@ -10,7 +10,11 @@ const Link = ({ children, to, ...rest }) => {
   };
 
   return (
-    <a className='text-blue-800 underline' href={to} onClick={handleClick} {...rest}>
+    <a
+      className={`text-blue-800 underline p-2${location === to ? ' font-bold border-l border-sky-500' : ''}`}
+      href={to}
+      onClick={handleClick}
+      {...rest}>
       {children}
     </a>
   );
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,12 +1,21 @@
 import Link from '../Link/Link';
 
 const SideBar = () => {
+  const createLink = (label, path) => ({ label, path });
+
+  const links = [
+    createLink('Accordion', '/accordion'),
+    createLink('Button', '/button'),
+    createLink('Dropdown', '/dropdown'),
+    createLink('Slider', '/slider')
+  ];
   return (
     <div className='sidebar bg-slate-300 p-6 mr-6 flex flex-col'>
-      <Link to='/accordion'>Accordion</Link>
-      <Link to='/button'>Button</Link>
-      <Link to='/dropdown'>Dropdown</Link>
-      <Link to='/slider'>Slider</Link>
+      {links.map(({ label, path }) => (
+        <Link key={path} to={path}>
+          {label}
+        </Link>
+      ))}
     </div>
   );
 };
