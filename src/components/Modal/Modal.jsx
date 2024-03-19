cba86592d177ae5366e451ae9317df5d92298d68
Dan Moffat <danmoffat@gmail.com>
2024-03-19 16:09:52 +0000

    Added cursor styling

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -19,8 +19,8 @@ const Modal = ({ message }) => {
         createPortal(
           <div
             onClick={handleClick}
-            className='fixed bg-black/50 top-0 left-0 w-full h-full flex items-center justify-center'>
-            <div className='bg-white p-8' onClick={e => e.stopPropagation()}>
+            className='fixed cursor-pointer bg-black/50 top-0 left-0 w-full h-full flex items-center justify-center'>
+            <div className='bg-white p-8 cursor-default' onClick={e => e.stopPropagation()}>
               {message}
             </div>
           </div>,
