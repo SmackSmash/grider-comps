1ffbbe2223514f56f404c6ed403f3543c572bfac
Dan Moffat <danmoffat@gmail.com>
2024-03-19 16:11:41 +0000

    A little styling

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -20,7 +20,7 @@ const Modal = ({ message }) => {
           <div
             onClick={handleClick}
             className='fixed cursor-pointer bg-black/50 top-0 left-0 w-full h-full flex items-center justify-center'>
-            <div className='bg-white p-8 cursor-default' onClick={e => e.stopPropagation()}>
+            <div className='bg-white p-8 cursor-default rounded' onClick={e => e.stopPropagation()}>
               {message}
             </div>
           </div>,
