7d86b21d6cd52022d98eac99ece1bdfc74df9ed8
Dan Moffat <danmoffat@gmail.com>
2024-03-19 02:39:01 +0000

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useContext } from 'react';
+import { NavigationContext } from './context/NavigationContext';
 import ButtonPage from './pages/ButtonPage/ButtonPage';
 import AccordionPage from './pages/AccordionPage/AccordionPage';
 import SideBar from './components/SideBar/SideBar';
@@ -5,15 +7,17 @@ import DropdownPage from './pages/DropdownPage/DropdownPage';
 import SliderPage from './pages/SliderPage/SliderPage';
 
 const App = () => {
-  document.title = window.location.pathname[1].toUpperCase() + window.location.pathname.slice(2) + ' Page';
+  // document.title = window.location.pathname[1].toUpperCase() + window.location.pathname.slice(2) + ' Page';
+  const { location } = useContext(NavigationContext);
 
   return (
     <div className='flex'>
       <SideBar />
-      <AccordionPage />
-      <ButtonPage />
-      <DropdownPage />
-      <SliderPage />
+      {location === '/' && <h1 className='text-slate-300'>Selct an option from the sidebar</h1>}
+      {location === '/accordion' && <AccordionPage />}
+      {location === '/button' && <ButtonPage />}
+      {location === '/dropdown' && <DropdownPage />}
+      {location === '/slider' && <SliderPage />}
     </div>
   );
 };
