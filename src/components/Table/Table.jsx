e720ebda8939b1226165df030fde78000b82a30b
Dan Moffat <danmoffat@gmail.com>
2024-03-20 02:27:51 +0000

    Added naive approach to displaying table data

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,13 +2,22 @@ import PropTypes from 'prop-types';
 
 const Table = ({ data }) => {
   return (
-    <div>
-      {data.map(({ name, color, score }, index) => (
-        <p key={index} className='text-slate-300'>
-          {name} {color} {score}
-        </p>
-      ))}
-    </div>
+    <table className='text-slate-300'>
+      <thead>
+        {Object.keys(data[0]).map(key => (
+          <th key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}</th>
+        ))}
+      </thead>
+      <tbody>
+        {data.map(({ name, color, score }) => (
+          <tr key={name}>
+            <td>{name}</td>
+            <td>{color}</td>
+            <td>{score}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
