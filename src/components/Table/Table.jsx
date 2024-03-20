342f41821a2f46105e253f0f20a0c0280f9609f2
Dan Moffat <danmoffat@gmail.com>
2024-03-20 02:12:19 +0000

    Added some dummy data to the new table component

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,5 +1,19 @@
-const Table = () => {
-  return <div>Table Works!</div>;
+import PropTypes from 'prop-types';
+
+const Table = ({ data }) => {
+  return (
+    <div>
+      {data.map(({ name, color, score }, index) => (
+        <p key={index} className='text-slate-300'>
+          {name} {color} {score}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+Table.propTypes = {
+  data: PropTypes.array
 };
 
 export default Table;
--- src/pages/TablePage/TablePage.jsx
+++ src/pages/TablePage/TablePage.jsx
@@ -1,10 +1,19 @@
 import Table from '../../components/Table/Table';
 
+const createData = (name, color, score) => ({ name, color, score });
+
+const data = [
+  createData('Orange', 'bg-orange', 5),
+  createData('Apple', 'bg-red-500', 3),
+  createData('Banana', 'bg-yellow-500', 1),
+  createData('Lime', 'bg-green-500', 4)
+];
+
 const TablePage = () => {
   return (
     <div>
       <h1 className='text-slate-300'>TABLE</h1>
-      <Table />
+      <Table data={data} />
     </div>
   );
 };
