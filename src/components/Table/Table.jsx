5fadfd6a8b92ead695713a7cca99f27ea74b0cb2
Dan Moffat <danmoffat@gmail.com>
2024-03-20 02:28:48 +0000

    Small amendment to add missing tr element in th

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -4,9 +4,11 @@ const Table = ({ data }) => {
   return (
     <table className='text-slate-300'>
       <thead>
-        {Object.keys(data[0]).map(key => (
-          <th key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}</th>
-        ))}
+        <tr>
+          {Object.keys(data[0]).map(key => (
+            <th key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}</th>
+          ))}
+        </tr>
       </thead>
       <tbody>
         {data.map(({ name, color, score }) => (
