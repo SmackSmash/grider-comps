c1323feaa95bdb97a538fc3bd6230f94ecd442a9
Dan Moffat <danmoffat@gmail.com>
2024-03-21 22:39:40 +0000

    Changed alphabetical sort algorithm to sigle line nexted terniary function

--- src/pages/TablePage/TablePage.jsx
+++ src/pages/TablePage/TablePage.jsx
@@ -6,7 +6,8 @@ const createData = (name, color, score) => ({ name, color, score });
 const TablePage = () => {
   const [data, setData] = useState([
     createData('Orange', 'bg-orange-500', 5),
-    createData('Apple', 'bg-red-500', 3),
+    createData('Apricot', 'bg-amber-500', 3),
+    createData('Apple', 'bg-red-600', 2),
     createData('Banana', 'bg-yellow-500', 1),
     createData('Lime', 'bg-green-500', 4)
   ]);
@@ -15,7 +16,7 @@ const TablePage = () => {
     {
       label: 'Fruit',
       render: fruit => fruit.name,
-      sort: () => setData([...data].sort((a, b) => (a.name[0] < b.name[0] ? -1 : 1)))
+      sort: () => setData([...data].sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0)))
     },
     { label: 'Color', render: fruit => <div className={`w-6 h-6 ${fruit.color}`}></div> },
     { label: 'Score', render: fruit => fruit.score, sort: () => setData([...data].sort((a, b) => a.score - b.score)) }
