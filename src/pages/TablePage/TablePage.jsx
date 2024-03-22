b8365763b62177c099cc4d93ff52c3b2cdcd7d72
Dan Moffat <danmoffat@gmail.com>
2024-03-22 17:51:00 +0000

    Added some more fruit and a nice border to the color palette

--- src/pages/TablePage/TablePage.jsx
+++ src/pages/TablePage/TablePage.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import Table from '../../components/Table/Table';
 
-const createData = (name, color, score) => ({ name, color, score });
+const createFruit = (name, color, score) => ({ name, color, score });
 
 const TablePage = () => {
   const [data, setData] = useState([
-    createData('Orange', 'bg-orange-500', 5),
-    createData('Apricot', 'bg-amber-500', 3),
-    createData('Apple', 'bg-red-600', 2),
-    createData('Banana', 'bg-yellow-500', 1),
-    createData('Lime', 'bg-green-500', 4)
+    createFruit('Orange', 'bg-orange-500', 5),
+    createFruit('Apricot', 'bg-amber-500', 3),
+    createFruit('Lemon', 'bg-yellow-300', 6),
+    createFruit('Apple', 'bg-red-600', 2),
+    createFruit('Banana', 'bg-yellow-500', 1),
+    createFruit('Lime', 'bg-green-500', 4),
+    createFruit('Blueberry', 'bg-indigo-950', 7)
   ]);
 
   const config = [
@@ -18,7 +20,7 @@ const TablePage = () => {
       render: fruit => fruit.name,
       sort: () => setData([...data].sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0)))
     },
-    { label: 'Color', render: fruit => <div className={`w-6 h-6 ${fruit.color}`}></div> },
+    { label: 'Color', render: fruit => <div className={`w-6 h-6 border border-slate-500 ${fruit.color}`}></div> },
     { label: 'Score', render: fruit => fruit.score, sort: () => setData([...data].sort((a, b) => a.score - b.score)) }
   ];
 
