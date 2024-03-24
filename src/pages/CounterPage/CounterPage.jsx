9365b47002ba28402c32146e1cfaf703d4fc8021
Dan Moffat <danmoffat@gmail.com>
2024-03-24 20:16:28 +0000

    Added basic logic for counter page

--- src/pages/CounterPage/CounterPage.jsx
+++ src/pages/CounterPage/CounterPage.jsx
@@ -1,7 +1,37 @@
+import { useState } from 'react';
+import Button from '../../components/Button/Button';
+
 const CounterPage = () => {
+  const [count, setCount] = useState(0);
+  const [addition, setAddition] = useState('');
+
+  const handleClick = num => {
+    setCount(count + num);
+  };
+
+  const handleChange = e => {
+    setAddition(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setCount(count + Number(addition));
+  };
+
   return (
     <div>
       <h1 className='text-slate-300 flex'>COUNTER</h1>
+      <h2 className='text-slate-300 text-6xl'>{count}</h2>
+      <Button success onClick={() => handleClick(1)}>
+        Increment
+      </Button>
+      <Button danger onClick={() => handleClick(-1)}>
+        Decrement
+      </Button>
+      <form onSubmit={handleSubmit}>
+        <input type='number' value={addition} onChange={handleChange} />
+        <Button primary>{addition ? `Add ${addition}` : `Add`}</Button>
+      </form>
     </div>
   );
 };
