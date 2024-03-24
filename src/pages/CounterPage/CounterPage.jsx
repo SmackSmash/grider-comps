f9972bf74b5b8bd7b5404ca723e0fea69aed1251
Dan Moffat <danmoffat@gmail.com>
2024-03-24 21:15:24 +0000

    Refactored counter to use useReducer for state

--- src/pages/CounterPage/CounterPage.jsx
+++ src/pages/CounterPage/CounterPage.jsx
@@ -1,28 +1,53 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useReducer } from 'react';
 import Button from '../../components/Button/Button';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { ...state, count: state.count + 1 };
+    case 'decrement':
+      return { ...state, count: state.count - 1 };
+    case 'set_addition':
+      return { ...state, addition: action.payload };
+    case 'add_many':
+      return { count: state.count + state.addition, addition: '' };
+    default:
+      return state;
+  }
+};
+
 const CounterPage = () => {
-  const [count, setCount] = useState(0);
-  const [addition, setAddition] = useState('');
+  // const [count, setCount] = useState(0);
+  // const [addition, setAddition] = useState('');
+  const [state, dispatch] = useReducer(reducer, { count: 0, addition: '' });
 
-  const handleIncrement = () => setCount(count + 1);
+  const handleIncrement = () => {
+    // setCount(count + 1);
+    dispatch({ type: 'increment' });
+  };
 
-  const handleDecrement = () => setCount(count - 1);
+  const handleDecrement = () => {
+    // setCount(count - 1);
+    dispatch({ type: 'decrement' });
+  };
 
   const handleChange = e => {
-    setAddition(e.target.value);
+    // setAddition(e.target.value);
+    dispatch({ type: 'set_addition', payload: Number(e.target.value) });
   };
 
   const handleSubmit = e => {
     e.preventDefault();
-    setAddition('');
-    setCount(count + Number(addition));
+    // setAddition('');
+    // setCount(count + Number(addition));
+    dispatch({ type: 'add_many' });
   };
 
   return (
     <div>
       <h1 className='text-slate-300 flex'>COUNTER</h1>
-      <h2 className='text-slate-300 text-6xl'>{count}</h2>
+      <h2 className='text-slate-300 text-6xl'>{state.count}</h2>
       <div className='flex row gap-2'>
         <Button danger onClick={handleDecrement}>
           Decrement
@@ -32,9 +57,9 @@ const CounterPage = () => {
         </Button>
       </div>
       <form onSubmit={handleSubmit}>
-        <input type='number' className='p-2' placeholder='Add a lot' value={addition} onChange={handleChange} />
+        <input type='number' className='p-2' placeholder='Add a lot' value={state.addition} onChange={handleChange} />
         <Button type='submit' primary>
-          {addition ? `Add ${addition}` : `Add`}
+          {state.addition ? `Add ${state.addition}` : `Add`}
         </Button>
       </form>
     </div>
